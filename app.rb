b144ad712a17bc1beeaf9a5466b4dc8fa651f435
Cliff <cliffhazelton@gmail.com>
2019-03-15 13:51:27 -0400

    Adds a config.ru file

--- app.rb
+++ app.rb
@@ -2,3 +2,25 @@ require 'sinatra'
 require 'json'
 require 'active_record'
 require 'sqlite3'
+
+=begin
+    rvm install 2.5.0
+    rvm use 2.5.0
+    gem install bundler
+    bundle install
+=end
+
+class App < Sinatra::Base
+
+    ActiveRecord::Base.establish_connection(
+        adapter:  'sqlite3',
+        database: 'app.db'
+    )
+
+    get "/players" do
+        @players = []
+
+        erb :players
+    end
+
+end
