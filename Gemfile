a19090e7f359c4398aaa46c873435ebf8a994f92
Cliff <cliffhazelton@gmail.com>
2019-03-15 13:10:59 -0400

    Removes the 'rake' gem

--- Gemfile
+++ Gemfile
@@ -3,5 +3,4 @@ source 'https://rubygems.org' do
   gem 'sinatra',      '~> 2.0.0'
   gem 'activerecord', '6.0.0.beta3'
   gem 'sqlite3',      '~> 1.3.6'
-  gem 'rake',         '~> 12.3.2'
 end
--- create_database.rb
+++ create_database.rb
@@ -2,26 +2,40 @@ require "sqlite3"
 
 db = SQLite3::Database.new("app.db")
 
-db.execute <<-SQL
+result = db.execute <<-SQL
   DROP TABLE IF EXISTS players
 SQL
 
-db.execute <<-SQL
+if result == []
+  puts "dropped players"
+end
+
+result = db.execute <<-SQL
   DROP TABLE IF EXISTS scores
 SQL
 
-db.execute <<-SQL
+if result == []
+  puts "dropped scores"
+end
+
+result = db.execute <<-SQL
   CREATE TABLE players (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(30)
   );
 SQL
 
-db.execute <<-SQL
+if result == []
+  puts "created players"
+end
+
+result = db.execute <<-SQL
   CREATE TABLE scores (
     value INTEGER,
     player_id INTEGER
   );
 SQL
 
-
+if result == []
+  puts "created scores"
+end
